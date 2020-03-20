9694f092b7acbec4960475ba1042d837dfff2ab8
EmanuelGF <emanuelfaisca@gmail.com>
2020-03-20 14:56:52 +0000

    Added NonNull atribute to the mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,7 +9,9 @@ const {
         GraphQLInt,
         GraphQLSchema,
         GraphQLID,
-        GraphQLList
+        GraphQLList,
+        GraphQLNonNull //For fields that cannot be null.
+
     } = graphql;
 
 //test data - this is done this way only for example purposes. In a real application the data should be persisted in 
@@ -128,8 +130,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: {type: GraphQLString},
-                age: { type: GraphQLInt}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                age: { type: new GraphQLNonNull(GraphQLInt)}
             },
             resolve(parent, args) {
                 let author = {
@@ -146,9 +148,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: {type: GraphQLString},
-                genre: { type: GraphQLString},
-                authorId: {type: GraphQLString}
+                name: {type: new GraphQLNonNull(GraphQLString)}, 
+                genre: { type: new GraphQLNonNull(GraphQLString)},
+                authorId: {type: new GraphQLNonNull(GraphQLString)}
             },
             resolve(parent, args) {
                 let book = {

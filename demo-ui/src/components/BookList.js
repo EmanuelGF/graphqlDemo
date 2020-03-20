6b793ce74f503a468b5933123a021ae491bfb524
EmanuelGF <emanuelfaisca@gmail.com>
2020-03-20 15:35:03 +0000

    set up the ui and added the booklist component

---
+++ demo-ui/src/components/BookList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function BookList() {
+    return (
+        <div>
+            <ul id="book-list">
+                from the booklist component
+            </ul>
+
+        </div>
+    )
+}
+
+export default BookList; 

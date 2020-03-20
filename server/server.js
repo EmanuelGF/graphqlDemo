c53e51ae79997e564c915f5470e70d547f0105f1
EmanuelGF <emanuelfaisca@gmail.com>
2020-03-20 15:03:32 +0000

    Added comments to the server file

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const app = express()
 
+/**Require the Graphql schema file  */
 const schema = require('./schema/schema')
 
 // Graphql setup
 const graphqlHTTP = require('express-graphql');
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: true
+    graphiql: true // Used to test the queries.
 }))
 
 
 
-const PORT = process.env.PORT || 8888
+const PORT = process.env.PORT || 8888  
 app.listen(PORT, ()=> {
     console.log(`Listening on port: ${PORT}`)
 })

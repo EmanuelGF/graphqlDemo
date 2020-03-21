902bd278e577b56085e8356335389bc07f1e8d31
EmanuelGF <emanuelfaisca@gmail.com>
2020-03-21 11:38:23 +0000

    Added Appolo to be able to retrieve data from the graphql server

--- demo-ui/src/App.js
+++ demo-ui/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import AppolloClient from 'apollo-boost'
+import {ApolloProvider} from '@apollo/react-hooks'
 
 //components
 import BookList from './components/BookList'
 
+//Appollo client setup
+const client = new AppolloClient({
+  uri: 'http://localhost:8888/graphql'
+})
+
+
+
 function App() {
   return (
-    <div id="main">
-      <h2>Graphql demo </h2>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h2>Graphql demo ✔</h2>
+        <BookList />
+      </div>
+    </ApolloProvider>
+    
   );
 }
 

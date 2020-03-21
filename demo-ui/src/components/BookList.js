96205f93c0f8baa9161d45d0b949b20db8eda5d8
EmanuelGF <emanuelfaisca@gmail.com>
2020-03-21 15:59:30 +0000

    Refactored the Booklist component

--- demo-ui/src/components/BookList.js
+++ demo-ui/src/components/BookList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {gql} from 'apollo-boost'
 import {useQuery} from '@apollo/react-hooks'
 
+//Queries
 const getBooksQuery = gql`
     {
         books{
@@ -13,29 +14,31 @@ const getBooksQuery = gql`
             }
         }
     }
-
 `
 
 function BookList() {
     const {loading, error, data} = useQuery(getBooksQuery);
 
-    if (loading) return <p>Loading...</p>;
+    
     if (error) return <p>Error :(</p>;
-
-    return (
-        <div>
-            {data.books.map((book, author) => (
+    if (loading) {
+        return <p>Loading Books</p>;
+    } else {
+        return (
+            <div>
+                {data.books.map((book) => (
+                    
+                    <ul id="book-list" key={book.id}>
+                        <li><b>Title:</b> {book.name}</li>
+                        <li><b>Genre:</b> {book.genre}</li>
+                        <li><b>By:</b> {book.author.name}</li>
+                    </ul>
+                ))}
                 
-                <ul id="book-list" key={book.id}>
-                    <li><b>Title:</b> {book.name}</li>
-                    <li><b>Genre:</b> {book.genre}</li>
-                    <li><b>By:</b> {book.author.name}</li>
-
-                </ul>
-            ))}
-            
-        </div>
-    )
+            </div>
+        )
+    }
+    
 }
 
 export default BookList; 

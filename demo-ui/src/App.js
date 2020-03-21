43362eb116293b5031c0a1fc5be372b884d2f1d1
EmanuelGF <emanuelfaisca@gmail.com>
2020-03-21 11:01:21 +0000

    minor change to the components

--- demo-ui/src/App.js
+++ demo-ui/src/App.js
@@ -6,7 +6,7 @@ import BookList from './components/BookList'
 function App() {
   return (
     <div id="main">
-      <h2>Hello world</h2>
+      <h2>Graphql demo </h2>
       <BookList />
     </div>
   );
